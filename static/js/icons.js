68f2cacc4371ee79b45a823e43d190b278392ae3
Artem Veriuzhskii <a.veriuzhskii@gmail.com>
2026-04-13 02:02:22 +0300

    feat: add "user" icon

--- static/js/icons.js
+++ static/js/icons.js
@@ -54,5 +54,13 @@ export const icons = {
         <path d="M3.714 3.048a.498.498 0 0 0-.683.627l2.843 7.627a2 2 0 0 1 0 1.396l-2.842 7.627a.498.498 0 0 0 .682.627l18-8.5a.5.5 0 0 0 0-.904z"/>
         <path d="M6 12h16"/>
     </svg>
+  `,
+  user: `
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+        aria-hidden="true">
+        <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+        <circle cx="12" cy="7" r="4"></circle>
+      </svg>
   `
 };
