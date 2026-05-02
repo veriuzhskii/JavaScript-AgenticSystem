641a3b08843321c121a1de52df1a6bec3ac4b905
Artem Veriuzhskii <a.veriuzhskii@gmail.com>
2026-05-02 18:07:30 +0300

    feat: add help icon for the guide button

--- static/js/icons.js
+++ static/js/icons.js
@@ -64,6 +64,14 @@ export const icons = {
       </svg>
   `,
 
+  help: `
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+      <circle cx="12" cy="12" r="10"></circle>
+      <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
+      <path d="M12 17h.01"></path>
+    </svg>
+  `,
+
   copy: `
     <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
       <rect width="14" height="14" x="8" y="8" rx="2" ry="2"></rect>
